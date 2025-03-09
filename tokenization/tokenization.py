b0b7e5ac13e9c8e5c97204f1c15d27e4906fa078
sayedseu <sayedhasanseu@gmail.com>
2025-03-09 17:43:53 +0600

    Initial commit

---
+++ tokenization/tokenization.py
@@ -0,0 +1,28 @@
+import spacy
+from torchtext.data import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+dataset = [
+    (1, "Introduction to NLP"),
+    (2, "Basics of PyTorch"),
+    (1, "NLP Techniques for Text Classification"),
+    (3, "Named Entity Recognition with PyTorch"),
+    (3, "Sentiment Analysis using PyTorch"),
+    (3, "Machine Translation with PyTorch"),
+    (1, " NLP Named Entity,Sentiment Analysis,Machine Translation "),
+    (1, " Machine Translation with NLP "),
+    (1, " Named Entity vs Sentiment Analysis  NLP ")]
+
+tokenizer = get_tokenizer("basic_english")
+
+
+def yield_tokens(data_iter):
+    for _, text in data_iter:
+        yield tokenizer(text)
+
+
+my_iterator = yield_tokens(dataset)
+
+vocab = build_vocab_from_iterator(my_iterator, specials=["<unk>"])
+vocab.set_default_index(vocab["<unk>"])
+print(vocab.get_stoi())
